3471e521297280863cdc623c962c3297fe430a4f
shreyacodes991 <tsara200p@gmail.com>
2024-10-28 00:06:54 +0530

    relational c.c

--- relational c.c
+++ relational c.c
@@ -6,16 +6,15 @@ Welcome to GDB Online.
   Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
+//code for relational operator
 #include <stdio.h>
 
 int main() {
-    int a, b;
+    int a = 15, b = 20; //declaration and defining variables a and b
 
-    // Input values from the user
-    printf("Enter the first number (a): ");
-    scanf("%d", &a);
-    printf("Enter the second number (b): ");
-    scanf("%d", &b);
+    // Display the values
+    printf("Value of a: %d\n", a);
+    printf("Value of b: %d\n", b);
 
     // Using relational operators and displaying the results
     printf("\nResults of relational operations:\n");
@@ -26,5 +25,5 @@ int main() {
     printf("a <= b : %d\n", a <= b);    // Checks if a is less than or equal to b
     printf("a >= b : %d\n", a >= b);    // Checks if a is greater than or equal to b
 
-    return 0;
+    return 0; //ends the execution of a function and returns control to the point where the function was called
 }
